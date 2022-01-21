4e8b410ddc17b91e0fd40ad90b3666f76ec8a7d1
bmmcq <chenqiang.mcq@alibaba-inc.com>
2022-01-21 13:36:01 +0800

    bug fix

--- research/engine/pegasus/pegasus/src/worker_id.rs
+++ research/engine/pegasus/pegasus/src/worker_id.rs
@@ -139,13 +139,21 @@ pub fn guard(worker_id: WorkerId) -> CurWorkerGuard {
 
 #[inline]
 fn set_current_worker(worker_id: Option<WorkerId>) {
-    CURRENT_WORKER.with(|w| w.set(worker_id))
+    CURRENT_WORKER.with(|w| {
+        let addr = w as *const Cell<Option<WorkerId>> as usize;
+        println!("set addr = 0x{}", addr);
+        w.set(worker_id)
+    })
 }
 
 #[inline]
 pub fn get_current_worker() -> WorkerId {
     CURRENT_WORKER
-        .with(|w| w.get())
+        .with(|w| {
+            let addr = w as *const Cell<Option<WorkerId>> as usize;
+            println!("get addr = 0x{}", addr);
+            w.get()
+        })
         .expect("current worker lost;")
 }
 
